4a25df9544f882dfb41b2fbfa8209111b9bddaa8
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-04-17 15:29:37 +0530

    Dark mode added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
+
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import About from './components/About';
+import { useState } from 'react';
+
 
 function App() {
+  const [mode, setDarkMode] = useState('light')
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+    setDarkMode('dark')
+    document.body.style.backgroundColor ='#343a40'
+    document.body.style.color ='#ffffff'
+    
+
+  }
+    else{
+      setDarkMode('light')
+      document.body.style.backgroundColor ='white'
+      document.body.style.color ='#000000'
+    
+    }
+  }
   return (
    <>
-<Navbar title="TextUtils"/>
+<Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
 <div className="container my-3">
-  <Textform heading="Enter the text to analyze"/>
+  <Textform heading="Enter the text to analyze" mode={mode}/>
 </div>
 {/* <About/> */}
    </>
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -2,14 +2,24 @@ import React, {useState} from 'react'
 
 
 export default function Textform(props) {
+  
+  const handleCopy = ()=>{
+    var text = document.getElementById("myBox")
+    text.select()
+    navigator.clipboard.writeText(text.value)
+  }
+
   const [text,setText] = useState('Enter text here')
   return (
     <>
       <div className="mb-3">
         <label htmlFor="myBox" className="form-label">{props.heading}</label>
-        <textarea className="form-control" id="myBox" rows="8" value={text} onChange={(event)=>setText(event.target.value)}></textarea>
+        <textarea className="form-control" id="myBox" rows="8" style={{backgroundColor:props.mode==='dark'?'#212529':'white', color:props.mode==='light'?'#212529':'white'}} value={text} onChange={(event)=>setText(event.target.value)}></textarea>
         <button className="btn btn-primary mx-3 my-3" onClick={()=>setText(text.toUpperCase())}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-3 my-3" onClick={()=>setText(text.toLowerCase())}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-3 my-3" onClick={handleCopy}>Copy text</button>
+        <button className="btn btn-primary mx-3 my-3" onClick={()=>setText(text.split(/[ ]+/).join(" "))}>Remove extra spaces</button>
+    
       </div>
       <div className="mb-3">
 
