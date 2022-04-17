4a89d5c47f881de9a5aa11a3c36c924a377e450b
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-04-17 16:56:10 +0530

    Alert component added

--- src/App.js
+++ src/App.js
@@ -5,30 +5,43 @@ import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import About from './components/About';
 import { useState } from 'react';
+import Alert from './components/Alert';
 
 
 function App() {
   const [mode, setDarkMode] = useState('light')
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+  }
   const toggleMode = ()=>{
     if(mode === 'light'){
     setDarkMode('dark')
     document.body.style.backgroundColor ='#343a40'
     document.body.style.color ='#ffffff'
-    
+    showAlert("Darkmode enabled","success")
 
   }
     else{
       setDarkMode('light')
       document.body.style.backgroundColor ='white'
       document.body.style.color ='#000000'
-    
+      showAlert("Darkmode disabled","success")
+
     }
   }
   return (
    <>
 <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+<div className="container">
+<Alert alert={alert}/>
+</div>
 <div className="container my-3">
-  <Textform heading="Enter the text to analyze" mode={mode}/>
+  <Textform showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>
 </div>
 {/* <About/> */}
    </>
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -7,6 +7,7 @@ export default function Textform(props) {
     var text = document.getElementById("myBox")
     text.select()
     navigator.clipboard.writeText(text.value)
+    props.showAlert("Text Copied","success")
   }
 
   const [text,setText] = useState('Enter text here')
@@ -15,8 +16,8 @@ export default function Textform(props) {
       <div className="mb-3">
         <label htmlFor="myBox" className="form-label">{props.heading}</label>
         <textarea className="form-control" id="myBox" rows="8" style={{backgroundColor:props.mode==='dark'?'#212529':'white', color:props.mode==='light'?'#212529':'white'}} value={text} onChange={(event)=>setText(event.target.value)}></textarea>
-        <button className="btn btn-primary mx-3 my-3" onClick={()=>setText(text.toUpperCase())}>Convert to Uppercase</button>
-        <button className="btn btn-primary mx-3 my-3" onClick={()=>setText(text.toLowerCase())}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-3 my-3" onClick={()=>{setText(text.toUpperCase()); props.showAlert("Text converted to uppercase","success")}}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-3 my-3" onClick={()=>{setText(text.toLowerCase()); props.showAlert("Text converted to lower","success")}}>Convert to Lowercase</button>
         <button className="btn btn-primary mx-3 my-3" onClick={handleCopy}>Copy text</button>
         <button className="btn btn-primary mx-3 my-3" onClick={()=>setText(text.split(/[ ]+/).join(" "))}>Remove extra spaces</button>
     
